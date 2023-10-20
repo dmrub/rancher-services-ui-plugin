8f5a19e693b34b57c8cb94bf486d50d4f1f6997c
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2023-10-20 16:15:48 +0200

    Fix error if service specification has no ports
    Check that ports is an array before iterating over it.

--- pkg/services-ui/components/ServicesDashboardCard.vue
+++ pkg/services-ui/components/ServicesDashboardCard.vue
@@ -66,84 +66,86 @@ export default {
         // console.log(`Check ${ serviceIndex }-th service ${ serviceNamespace }/${ serviceName }`);
         // console.log(service);
 
-        for (const port of service.spec.ports) {
-          if (port.protocol !== 'TCP') {
-            continue;
-          }
-
-          const portName = port.name ?? '';
-          const portNum = port.port ?? 0;
-
-          if (portNum <= 0) {
-            continue;
-          }
-
-          let isHttps = portName.includes('https');
-          let isHttp = portName.includes('http') && !isHttps;
-
-          if (!isHttp && !isHttps) {
-            switch (portNum) {
-            case 80:
-            case 8080:
-              isHttp = true;
-              break;
-            case 443:
-            case 8443:
-            case 6443:
-              isHttps = true;
-              break;
+        if (service.spec !== undefined && Array.isArray(service.spec.ports)) {
+          for (const port of service.spec.ports) {
+            if (port.protocol !== 'TCP') {
+              continue;
             }
-          }
 
-          if (!isHttp && !isHttps) {
-            continue;
-          }
+            const portName = port.name ?? '';
+            const portNum = port.port ?? 0;
 
-          await service.fetchPods();
+            if (portNum <= 0) {
+              continue;
+            }
 
-          const servicePort = portNum;
-          const serviceProtocol = isHttp ? 'http' : 'https';
-          const serviceDescription = isHttp ? `HTTP service on port ${ portNum }` : `HTTPS service on port ${ portNum }`;
-          const serviceId = `${ serviceNamespace }/${ serviceName }:${ portNum }`;
+            let isHttps = portName.includes('https');
+            let isHttp = portName.includes('http') && !isHttps;
 
-          const serviceLink = `/k8s/clusters/${ currentCluster.id }/api/v1/namespaces/${ serviceNamespace }/services/${ serviceProtocol }:${ serviceName }:${ servicePort }/proxy`;
-
-          let serviceLinkEnabled = true;
-          let serviceLinkStatus = '';
-
-          if (service.pods.length === 0) {
-            serviceLinkEnabled = false;
-            serviceLinkStatus = 'No endpoint pods';
-          } else {
-            try {
-              const response = await this.$axios.get(serviceLink, { timeout: 5000 });
-
-              serviceLinkEnabled = response && response.status === 200;
-
-              if (response) {
-                if (response.status !== 200) {
-                  serviceLinkStatus = `Could not get link, status code: ${ response.status.toString() }`;
-                }
-              } else {
-                serviceLinkStatus = 'Could not get link, no status code';
+            if (!isHttp && !isHttps) {
+              switch (portNum) {
+              case 80:
+              case 8080:
+                isHttp = true;
+                break;
+              case 443:
+              case 8443:
+              case 6443:
+                isHttps = true;
+                break;
               }
-            } catch (e) {
-              serviceLinkEnabled = false;
-              serviceLinkStatus = `Could not get link: ${ e.toString() }`;
             }
-          }
 
-          externalLinks.push({
-            enabled:     serviceLinkEnabled,
-            iconSrc:     icons.gear,
-            kind:        'service',
-            namespace:   serviceNamespace,
-            name:        serviceName,
-            description: serviceDescription,
-            id:          serviceId,
-            link:        serviceLink,
-            status:      serviceLinkStatus
-          });
+            if (!isHttp && !isHttps) {
+              continue;
+            }
+
+            await service.fetchPods();
+
+            const servicePort = portNum;
+            const serviceProtocol = isHttp ? 'http' : 'https';
+            const serviceDescription = isHttp ? `HTTP service on port ${ portNum }` : `HTTPS service on port ${ portNum }`;
+            const serviceId = `${ serviceNamespace }/${ serviceName }:${ portNum }`;
+
+            const serviceLink = `/k8s/clusters/${ currentCluster.id }/api/v1/namespaces/${ serviceNamespace }/services/${ serviceProtocol }:${ serviceName }:${ servicePort }/proxy`;
+
+            let serviceLinkEnabled = true;
+            let serviceLinkStatus = '';
+
+            if (service.pods.length === 0) {
+              serviceLinkEnabled = false;
+              serviceLinkStatus = 'No endpoint pods';
+            } else {
+              try {
+                const response = await this.$axios.get(serviceLink, { timeout: 5000 });
+
+                serviceLinkEnabled = response && response.status === 200;
+
+                if (response) {
+                  if (response.status !== 200) {
+                    serviceLinkStatus = `Could not get link, status code: ${ response.status.toString() }`;
+                  }
+                } else {
+                  serviceLinkStatus = 'Could not get link, no status code';
+                }
+              } catch (e) {
+                serviceLinkEnabled = false;
+                serviceLinkStatus = `Could not get link: ${ e.toString() }`;
+              }
+            }
+
+            externalLinks.push({
+              enabled:     serviceLinkEnabled,
+              iconSrc:     icons.gear,
+              kind:        'service',
+              namespace:   serviceNamespace,
+              name:        serviceName,
+              description: serviceDescription,
+              id:          serviceId,
+              link:        serviceLink,
+              status:      serviceLinkStatus
+            });
+          }
         }
       }
     },
